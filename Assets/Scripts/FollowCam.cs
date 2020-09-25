868c01dd517209f6ec7a45a54da4a5f8ca7cf916
shikamaru125 <70780812+shikamaru125@users.noreply.github.com>
2020-09-25 14:07:27 -0400

    Camera Follow/Projectile Collider Altered

---
+++ Assets/Scripts/FollowCam.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour
+{
+    static public GameObject POI; //the static point of interest
+
+    [Header("Set Dynamically")]
+    public float camZ; //the desired Z pos of the camera
+    void Awake()
+    {
+        camZ = this.transform.position.z;
+    }
+
+    void FixedUpdate() 
+    {
+        //if there's only one line following an if, it doesn't need braces
+        if(POI==null) return; //return if there is no poi
+
+        //get the position of the poi
+        Vector3 destination = POI.transform.position;
+        //Force destination.z to be camZ to keep the camera for enough away
+        destination.z = camZ;
+        //set the camera to the destination
+        transform.position = destination;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
